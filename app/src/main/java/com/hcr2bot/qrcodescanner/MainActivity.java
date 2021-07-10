41b6d140dbdf7e65325edb353753c1d6cb187c49
Abhay <abhaysingbhosale1@gmail.com>
2021-07-10 12:25:31 +0530

    Added feature to generate QR codes

--- app/src/main/java/com/hcr2bot/qrcodescanner/MainActivity.java
+++ app/src/main/java/com/hcr2bot/qrcodescanner/MainActivity.java
@@ -1,30 +1,68 @@
 package com.hcr2bot.qrcodescanner;
 
+import android.Manifest;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button scan;
+    Button scan, generate, share;
+    EditText editText;
+    ImageView imageView;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scan = findViewById(R.id.scan);
+        generate = findViewById(R.id.generate);
+        editText = findViewById(R.id.editText);
+        imageView = findViewById(R.id.imageView);
+        share = findViewById(R.id.shareImg);
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+            }
+        }
+        else { //you dont need to worry about these stuff below api level 23
+
+        }
+
 
         scan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +79,44 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
+        generate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (TextUtils.isEmpty(editText.getText().toString())){
+                    Toast.makeText(MainActivity.this, "Provided empty field", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String value = editText.getText().toString().trim();
+                    MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                    try {
+                        BitMatrix bitMatrix = multiFormatWriter.encode(value, BarcodeFormat.QR_CODE, 350, 350);
+                        BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                        bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                        imageView.setImageBitmap(bitmap);
+                    } catch (WriterException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               BitmapDrawable drawable = (BitmapDrawable)imageView.getDrawable();
+               Bitmap bitmap1 = drawable.getBitmap();
+               String bitmapPath = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap1, "title", null);
+               Uri uri  = Uri.parse(bitmapPath);
+               Intent intent = new Intent(Intent.ACTION_SEND);
+               intent.setType("image/png");
+               intent.putExtra(Intent.EXTRA_STREAM, uri);
+               intent.putExtra(Intent.EXTRA_TEXT, "Scan this QR code.");
+               startActivity(Intent.createChooser(intent, "Share"));
+            }
+            });
+
     }
 
     @Override
@@ -69,4 +145,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
+
+
 }
